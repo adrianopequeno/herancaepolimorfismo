6dd61ee882aaab9ebf30088e60cd21c33ab39b88
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-09-28 18:10:16 -0300

    Implementing the program principal for test the application.

--- taxpayers/src/application/Program.java
+++ taxpayers/src/application/Program.java
@@ -1,9 +1,63 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of taxpayers: ");
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Taxpayer #" + i + " data:");
+			System.out.print("Individual or company (i/c)? ");
+			char type = sc.next().charAt(0);
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Anual income: ");
+			Double anualIncome = sc.nextDouble();
+			
+			if (type == 'i') {
+				System.out.print("Health expenditures: ");
+				Double healthExpenditures = sc.nextDouble();
+				
+				list.add(new Individual(name, anualIncome, healthExpenditures));
+			} else {
+				System.out.print("Number of employees: ");
+				Integer numberOfEmployees = sc.nextInt();
+				
+				list.add(new Company(name, anualIncome, numberOfEmployees));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer taxp : list) {
+			System.out.println(taxp.getName() + ": $ " + String.format("%.2f", taxp.tax()));
+		}
+		
+		double sum = 0.0;
+		for (TaxPayer taxp : list) {
+			sum += taxp.tax();
+		}
+		
+		System.out.println();
+		System.out.printf("TOTAL TAXES: $ %.2f", sum);
+		
+		sc.close();
 
 	}
 
